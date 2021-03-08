575d1c3948f920213cb1bf0f6ead85dbb1eccdef
Jim Li <jli263@uic.edu>
2021-03-07 19:22:35 -0600

    Added to shopping path

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function coffeeChoice(event) {
     window.location = "cityCoffee.html";
 }
 
-function load_borders() {
+function load_coffee_borders() {
     if (localStorage.getItem("coffeeChoice").localeCompare("dunkin") == 0) {
         document.getElementById("b_left").src = "images/dunkin_left_border.jpg";
         document.getElementById("b_right").src = "images/dunkin_right_border.jpg";
@@ -16,5 +16,21 @@ function load_borders() {
     else {
         document.getElementById("b_left").src = "images/starbucks_left_border.jpg";
         document.getElementById("b_right").src = "images/starbucks_right_border.jpg";
+    }
+}
+
+function shoppingChoice(event) {
+    localStorage.setItem("shoppingChoice", event.value);
+    window.location = "cityShopping.html";
+}
+
+function load_shopping_borders() {
+    if (localStorage.getItem("shoppingChoice").localeCompare("Patagonia") == 0) {
+        document.getElementById("b_left").src = "images/patagonia_left_border.jpg";
+        document.getElementById("b_right").src = "images/patagonia_right_border.jpg";
+    }
+    else {
+        document.getElementById("b_left").src = "images/levis_left_border.jpg";
+        document.getElementById("b_right").src = "images/levis_right_border.jpg";
     }
 }
